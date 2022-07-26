3c92e2438447666080375753e4863fddc41c6f36
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-26 18:52:40 +0300

    Add. gitignore

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив строк. Напишите программу, считает кол-во слов в массиве, 
+// начинающихся на гласную букву.
+
+// Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
+
+string[] RandomWords ={"qwe", 
+                      "wer", 
+                      "ert", 
+                      "rty", 
+                      "tyu",
+                      "yuy",
+                      "asdf"};
+
+int FindWords(string[] words)
+{
+    int count = 0;
+    for(int i = 0; i < words.Length; i++)
+    {
+        if(words[i][0] == 'a'||
+           words[i][0] == 'e'||
+           words[i][0] =='y'||
+           words[i][0] == 'u'||
+           words[i][0] == 'i'||
+           words[i][0] == 'o')
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(FindWords(RandomWords));
---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив строк. Напишите программу, которая 
+// генерирует новый массив, объединяя элементы исходного массива попарно.
+
+// Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+
+string[] Massive = {"qwe", "wer", "ert", "rty", "tyu", "yui"};
+
+string[] AddElement(string[] massive)
+{
+    string[] result = new string[massive.Length /2];
+
+    for(int i =0; i < massive.Length/2; i++)
+    {
+        result[i] = massive[i]+ massive[i+1];
+    }
+    return result;
+}
+
+void ShowResult(string[] res)
+{
+    for(int i = 0; i < res.Length; i++)
+        Console.Write(res[i] + " ");
+}
+ShowResult(AddElement(Massive));
